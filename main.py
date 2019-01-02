8d7b0535539fb052ef5de9fdf3b28a2b13feac69
Ahmadreza <ahmadreza.smdi@gmail.com>
2019-01-02 12:41:28 +0330

    Creating the representation

---
+++ main.py
@@ -0,0 +1,48 @@
+
+from random import randint,choice
+
+
+
+#First we get number of points and players
+
+numberOfPoints = int(input('How many points do you need? \n'))
+numberOfPlayers = int(input('How many players ?'))
+
+nameOfPlayer = []
+for i in range(numberOfPlayers):
+    nameOfPlayer.append(input('Write name [%s] :'%i))
+
+# just create a rising list of numbers
+X_axis = []
+for i in range(numberOfPoints):
+    X_axis.append(i)
+    
+#chunks function for partitioning the points for players    
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+#Representation
+def Repre():
+    
+    RepreList = []
+    for i in range(numberOfPoints):
+        RepreList.append(randint(0,numberOfPoints))
+    
+    if int(numberOfPoints/numberOfPlayers)==numberOfPoints/numberOfPlayers:
+        divider = int(numberOfPoints/numberOfPlayers)
+    else:
+        divider = int(numberOfPoints/numberOfPlayers)+1
+
+    PlayerPoints = list(chunks(X_axis,divider))
+   
+    for i in range(len(PlayerPoints)):
+        RepreList.append(len(PlayerPoints[i]))
+    
+    for i in range(len(nameOfPlayer)):
+        
+        print(nameOfPlayer[i],':',PlayerPoints[i])
+
+    print(*RepreList,sep = ", ")
+
+Repre()
