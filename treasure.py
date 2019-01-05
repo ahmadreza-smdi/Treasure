5ed2237a3b31d47f621ce33487b7144543160676
Ahmadreza <ahmadreza.smdi@gmail.com>
2019-01-06 00:43:21 +0330

    update on game, updat on comments, cleansing the code

--- treasure.py
+++ treasure.py
@@ -48,7 +48,7 @@ class gameAdmin():
     def getPoints(self):
 
         NumberOfPoints = int(input("Number Of Points : \n"))
-        print('Options:')
+        print('Options:\n')
         print('Press 0 : Random enterance \nPress 1 : Entering point from keyboard')
         choice = int(input(''))
         if choice == 1:
@@ -61,7 +61,7 @@ class gameAdmin():
         if choice == 0:
             for i in range(NumberOfPoints):
                 city = Tools()
-                print('x[%s] %s'%(i,city.getX()),',','y[%s] %s'%(i,city.getY()),end=' ')
+                print('x[%s] %s'%(i,city.getX()),',','y[%s] %s'%(i,city.getY()),end='\n')
                 self.GamePoints.append(city)
         
     def numberOfTools(self):
@@ -117,7 +117,7 @@ class game:
         PlayerPoints = list(self.chunks(self.points,self.divider))
     
         for i in range(self.numberOfplayers):
-            print(self.players[i] + '"s Goal',end='=')
+            print(self.players[i] + " Goals",end='=')
             for j in range(len(PlayerPoints[i])):
                 print('[' + str(PlayerPoints[i][j].x) +','+ str(PlayerPoints[i][j].y) + ']' + '',end='')  
             print('\n')
@@ -148,7 +148,7 @@ class game:
 #we Distinguish between levels, 0 -> easy , 1 -> normal , 2 -> hard
 
 #We add each Fitness value to Fitness and by using index we map it to each answer
-    def fitnessFunction(self,level):
+    def fitnessFunction(self,level,maxFitness):
         self.Fitness=[]
         for i in range(len(self.FirstGeneration)): 
 
@@ -166,9 +166,14 @@ class game:
                             self.Fitness.append(1/eachFitness*eachFitness)            
             if level==2:
                             self.Fitness.append(1/eachFitness*eachFitness*eachFitness)
-        for i in range(len(self.Fitness)):
-            print(self.Fitness[i],sep=',')
+        # for i in range(len(self.Fitness)):
+        #     print(self.Fitness[i],sep=',')
+        if maxFitness==1:
+            return list(self.FirstGeneration[self.Fitness.index(max(self.Fitness))])
+            
 
+
+#Selection of answers, percent of finnest answers to go to next generation without cross over or mutation
 
     def selection(self,percentage):
         self.SelectedFirstGeneration = []
@@ -180,36 +185,36 @@ class game:
 
 
 
-        print('Selection')
-        for i in range(len(self.SelectedFirstGeneration)):
-            for j in range(len(self.points)):
-                print(self.SelectedFirstGeneration[i][j].x,',',self.SelectedFirstGeneration[i][j].y,end='')
-            print()
+        # print('Selection')
+        # for i in range(len(self.SelectedFirstGeneration)):
+        #     for j in range(len(self.points)):
+        #         print(self.SelectedFirstGeneration[i][j].x,',',self.SelectedFirstGeneration[i][j].y,end='')
+        #     print()
 
 
 
 
 
-
+#Cross over the two random answer and add to next generation
 
     def crossOver(self,number):
+        
         for i in range(number):
             obj1 = choice(self.FirstGeneration)
-
-            print('Object1:',end='')
-            for j in range(len(obj1)):
-                print(str(obj1[j].x)+','+str(obj1[j].y),end=' ')
-            print()
+            # print('Object1:',end='')
+            # for j in range(len(obj1)):
+            #     print(str(obj1[j].x)+','+str(obj1[j].y),end=' ')
+            # print()
 
 
 
 
             obj2 = choice(self.FirstGeneration)
 
-            print('Object2:',end='')
-            for j in range(len(obj1)):
-                print(str(obj2[j].x)+','+str(obj2[j].y),end=' ')
-            print()
+            # print('Object2:',end='')
+            # for j in range(len(obj1)):
+            #     print(str(obj2[j].x)+','+str(obj2[j].y),end=' ')
+            # print()
 
 
             p1=[]
@@ -221,45 +226,37 @@ class game:
 
             for k in range(0,len(p1)):
                 p2.append(obj2.index(p1[k]))
-                print(obj2.index(p1[k]))
+                # print(obj2.index(p1[k]))
             p2.sort()
 
             p1InOrder= []            
             for k in range(len(p1)):
                 p1InOrder.append(obj1.index(p1[k]))
-                print('p1InOrder',p1InOrder)
+                # print('p1InOrder',p1InOrder)
             p1InOrder.sort()
-            print('sorted p1:',*p1InOrder)
+            # print('sorted p1:',*p1InOrder)
 
-            for t in range(len(p1)):
-                print(str(p1[t].x)+','+str(p1[t].y),end=' ')
-            print()
+            # for t in range(len(p1)):
+            #     print(str(p1[t].x)+','+str(p1[t].y),end=' ')
+            # print()
 
-            
+            #Main work
             for d in range(len(p2)):
-                print('len(p1)',len(p1))
-                print('____________________________________________')
-
-                print('obj1[p1InOrder]',obj1[p1InOrder[d]].x,':',obj1[p1InOrder[d]].y)
-                print('obj2[p2[d]]',obj2[p2[d]].x,':',obj2[p2[d]].y)
                 obj1[p1InOrder[d]] = obj2[p2[d]]
-
-                # print("obj1[obj1.index(p1[d])]",obj1[obj1.index(p1[d])].x,obj1[obj1.index(p1[d])].y)
                 
             self.SelectedFirstGeneration.append(obj1)
 
-            for i in range(len(self.SelectedFirstGeneration)):
-                for j in range(len(self.points)):
-                    print(self.SelectedFirstGeneration[i][j].x,',',self.SelectedFirstGeneration[i][j].y,end='  ')
-                print()
+            # for i in range(len(self.SelectedFirstGeneration)):
+            #     for j in range(len(self.points)):
+            #         print(self.SelectedFirstGeneration[i][j].x,',',self.SelectedFirstGeneration[i][j].y,end='  ')
+            #     print()
 
-            print('CrossOver',end='')
-            for j in range(len(obj1)):
-                print(str(obj1[j].x)+','+str(obj1[j].y),end=' ')
-            print() 
-            print(')))))))))))))))))))))))))))))))))))))))))))))))))))))))))))')
-        print()
-    
+        #     print('CrossOver',end='')
+        #     for j in range(len(obj1)):
+        #         print(str(obj1[j].x)+','+str(obj1[j].y),end=' ')
+        #     print() 
+        # print()
+#Mutation of two points in a answer    
     def mutation(self,percentage):
         
         for i in range(int((len(self.SelectedFirstGeneration)*percentage/100)+1)):
@@ -274,38 +271,47 @@ class game:
             
 
    
-        for i in range(len(self.SelectedFirstGeneration)):
-            for j in range(len(self.points)):
-                print(self.SelectedFirstGeneration[i][j].x,',',self.SelectedFirstGeneration[i][j].y,end='  ')
-            print()
+        # for i in range(len(self.SelectedFirstGeneration)):
+        #     for j in range(len(self.points)):
+        #         print(self.SelectedFirstGeneration[i][j].x,',',self.SelectedFirstGeneration[i][j].y,end='  ')
+        #     print()
 
 
+#Number of generation to create, it include selection,crossOver,mutation
     def generation(self,):
         numberOfGeneration = input('How many generations do you want to create?')
-        self.FirstGeneration = self.SelectedFirstGeneration
+        self.Hardness = int(input('Easy:0 Normal:1 Hard:2'))
+        print()
         for i in range(int(numberOfGeneration)):
-            print('Generation___________________%s'%i)
-            self.fitnessFunction(0)
+            self.fitnessFunction(self.Hardness,0)
             self.selection(30)
             self.crossOver(2)
             self.mutation(10)
         self.FirstGeneration = self.SelectedFirstGeneration
-        return self.SelectedFirstGeneration
+#Find best answer
+    def BestAnswer(self,):
+        for i in range(len(self.SelectedFirstGeneration)):
+            for j in range(len(self.SelectedFirstGeneration[0])):
+                print('['+str(self.SelectedFirstGeneration[i][j].x)+','+str(self.SelectedFirstGeneration[i][j].y)+']',end=' ')
+            print()
+        bestAns = self.fitnessFunction(self.Hardness,1)
+        print('Best Answer :\n')
+        for i in range(len(bestAns)):
+            print('['+str(bestAns[i].x)+','+str(bestAns[i].y)+']',end='')
+        print()
+def main():
+    print('\n\n\n_______________________________WELCOM TO TREASURE_______________________________')
+    admin = gameAdmin() 
+    admin.getPoints()
+    treasure = game(admin)
+    treasure.getPlayers()
+    treasure.createMap()
+    clear()
+    treasure.repre()
+    treasure.playerPoint()
+    treasure.population()
+    treasure.generation()
+    treasure.BestAnswer()
 
 
-
-ali = gameAdmin() 
-ali.getPoints()
-treasure = game(ali)
-treasure.getPlayers()
-treasure.createMap()
-clear()
-treasure.repre()
-treasure.playerPoint()
-treasure.population()
-treasure.fitnessFunction(0)
-
-treasure.selection(30)
-treasure.crossOver(2)
-treasure.mutation(10)
-treasure.generation()
+main()
